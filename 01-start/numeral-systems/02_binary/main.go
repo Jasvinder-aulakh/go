362bf5c5c9ba10e734aa948a9f5f8c2e6e66d371
Jasvinder Singh <jinghasvinders-air@Jasvinder-Air.local>
2019-05-29 17:02:36 +0530

    Added getting started

---
+++ 01-start/numeral-systems/02_binary/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b \n", 42, 42)
+
+}
---
+++ 01-start/numeral-systems/03_hexadecimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+
+}
