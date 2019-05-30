315439d36efc8a8178ddc9365c978c8d80a4c513
Jasvinder Singh <jinghasvinders-air@Jasvinder-Air.local>
2019-05-30 10:51:42 +0530

    Added getting started

---
+++ 01-start/numeral-systems/05_declare/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var y = 87
+
+var z int
+
+func main() {
+	// Declare the variable and assign a value
+	x := 43
+	fmt.Println(x)
+	fmt.Println(y)
+	z = x + y
+	fmt.Println(z)
+
+}
